c6d7553f4296b928c80b4b306b6fbf808acb3001
vanekm <martin.vanek@fykos.cz>
2024-01-17 21:31:16 +0100

    add option to only upload submits from people from abroad

--- pdfsplit.py
+++ pdfsplit.py
@@ -1,14 +1,40 @@
 import os, json, shutil
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from collections import OrderedDict
+import pandas as pd
 
-def init_upload_me(split_path, upload_me_path, problems):
+def init_upload_me(split_path, upload_me_path, problems, upload_foreign_only, submitids = None):
     back_up_path = upload_me_path + "/upload_me"
     if not os.path.exists(back_up_path):
         print("Vytvarim ./upload_me")
+        
+        # create the back_up_path directory
+        os.makedirs(back_up_path)
 
-        for problem in problems:
-            shutil.copytree(split_path + f'/uloha-{problem}', back_up_path + f'/uloha-{problem}')
+        if upload_foreign_only and submitids is not None:
+            for problem in problems:
+                all_files_for_problem = os.listdir(split_path + f'/uloha-{problem}')
+                for file in all_files_for_problem:
+                    file_sumbitid = file[file.find("__")+2:-4] # the -4 is to remove the .pdf
+                    try:
+                        file_sumbitid = int(file_sumbitid)
+                    except:
+                        print(f"file_sumbitid: {file_sumbitid} neni cislo, preskakuji")
+                        continue
+                    # print(f"file_sumbitid: {file_sumbitid}")
+                    
+                    if file_sumbitid in submitids:
+                        # if the folder does not exist, create it
+                        if not os.path.exists(back_up_path + f'/uloha-{problem}'):
+                            os.makedirs(back_up_path + f'/uloha-{problem}')
+                        shutil.copyfile(split_path + f'/uloha-{problem}/' + file, back_up_path + f'/uloha-{problem}/' + file)
+                        # print(f"Copying {split_path + f'/uloha-{problem}/' + file} to {back_up_path + f'/uloha-{problem}/' + file}")                
+            
+        else:
+            if submitids is None:
+                print("Zadano ze chces jen zahranicni, ale submitids nenalezeno -> ponechavam vsechny soubory")
+            for problem in problems:
+                shutil.copytree(split_path + f'/uloha-{problem}', back_up_path + f'/uloha-{problem}')
         
         print("upload_me vytvoren.\n")
     else:
@@ -67,18 +93,24 @@ def split_it(joined_and_split_dir, stranytxt_dir, problems):
         print("Strany nesedi v techto ulohach")
         print(kdenesouhlasi)
 
-    init_upload_me(joined_and_split_dir + f'/zaloha_split', joined_and_split_dir, problems)
-
 if __name__ == "__main__":
     
     problems = ["1","2","3","4","5","P","E","S"]
 
-
     rocnik = input('napis cislo rocniku: ')
     serie = input('napis cislo serie: ')
+    upload_foreign_only = input('Chces uploadovat jen zahranicni (z) nebo vse (v)?')
+    if upload_foreign_only == "z":
+        upload_foreign_only = True
+    else:
+        upload_foreign_only = False
 
     joined_and_split_dir = f'./corrected/rocnik{rocnik}/serie{serie}'
     stranytxt_dir = f"./download/rocnik{rocnik}/serie{serie}"
 
     split_it(joined_and_split_dir, stranytxt_dir, problems)
     
+    submitids = pd.read_csv(joined_and_split_dir + '/submitids.csv',sep=';')
+    submitids = submitids["submit"].values
+
+    init_upload_me(joined_and_split_dir + f'/zaloha_split', joined_and_split_dir, problems, upload_foreign_only, submitids)
