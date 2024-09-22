f83ea094da5045c935cbdb48674c5d7298efad50
vanekm <martin.vanek@fykos.cz>
2024-09-22 10:04:47 +0200

    separate czech and foreign solutions is 'joined' files

--- pdfjoin.py
+++ pdfjoin.py
@@ -1,18 +1,19 @@
-from PyPDF2 import PdfFileMerger, PdfFileReader
-import glob
 import os
+import glob
 import json
+import pandas as pd
 from collections import OrderedDict
+from PyPDF2 import PdfFileMerger, PdfFileReader
 
 def get_pages(file_name):
-    with open(file_name,'rb') as file:
+    with open(file_name, 'rb') as file:
         pdf = PdfFileReader(file) 
         pg = pdf.getNumPages()
     return pg
 
-def pdf_ok_writing(pdf, temp_path = "./temp"):
+def pdf_ok_writing(pdf, temp_path="./temp"):
     if not os.path.exists(temp_path):
-            os.makedirs(temp_path)
+        os.makedirs(temp_path)
     try:
         merger = PdfFileMerger()
         merger.append(pdf)
@@ -22,103 +23,103 @@ def pdf_ok_writing(pdf, temp_path = "./temp"):
     except:
         return False
 
+def is_foreign(file_name, foreign_submitids):
+    file_submitid = file_name[file_name.find("__")+2:-4]
+    try:
+        return int(file_submitid) in foreign_submitids
+    except:
+        return False
+
 def join_it(download_path, split_exceptions, problems):
     exceptions = []
-    white = 'white.pdf' 
+    white = 'white.pdf'
 
     if split_exceptions == "d" or split_exceptions == False:
         split_exceptions = False
     else:
         split_exceptions = True
 
-    for problem in problems:
+    # Read the foreign submitids
+    submitids_df = pd.read_csv(os.path.join(download_path, 'submitids.csv'), sep=';')
+    foreign_submitids = submitids_df["submit"].values
 
-        sumpg = 0
-        sum_whites = 0
+    for problem in problems:
         print(f'uloha: {problem}')
 
-        merger = PdfFileMerger()
-        path_list = glob.glob( download_path + f'/uloha-{problem}/*')
+        merger_czech = PdfFileMerger()
+        merger_foreign = PdfFileMerger()
+        path_list = glob.glob(download_path + f'/uloha-{problem}/*')
         pdf_list = [path for path in path_list if 'pdf' in path[-4:].lower()]
         pdf_list = sorted(pdf_list)
-        dictnarozdeleni = OrderedDict()
+        dictnarozdeleni_czech = OrderedDict()
+        dictnarozdeleni_foreign = OrderedDict()
 
         for pdf in pdf_list:
             if pdf_ok_writing(pdf):
-                pages = 0 #at v tom nezustava bordel
                 pages = get_pages(pdf)
-                sumpg += pages
-                merger.append(pdf)
-                if pages % 2 != 0:
-                    merger.append(white)
-                    sum_whites += 1
-                    pages += 1
-
-                #tohle potrebuju jen ty, co se daji mergnout, tzn. i rozmergnout
-                dictnarozdeleni.update( { pdf : pages } )
-
-            #pokud to nespocita pages, tak to predpokladam ani nemergne...
+                if is_foreign(pdf, foreign_submitids):
+                    merger_foreign.append(pdf)
+                    if pages % 2 != 0:
+                        merger_foreign.append(white)
+                        pages += 1
+                    dictnarozdeleni_foreign.update({pdf: pages})
+                else:
+                    merger_czech.append(pdf)
+                    if pages % 2 != 0:
+                        merger_czech.append(white)
+                        pages += 1
+                    dictnarozdeleni_czech.update({pdf: pages})
             else:
                 try: 
                     p = get_pages(pdf)
                     print("chyba writing: ", pdf)
                 except:
                     print("chyba get_pages: ", pdf)
-
                 exceptions.append(pdf)
 
+        joined_path_czech = download_path + f'/joined_uloha-{problem}_czech.pdf'
+        joined_path_foreign = download_path + f'/joined_uloha-{problem}_foreign.pdf'
         
-        joined_path = download_path + f'/joined_uloha-{problem}.pdf'
-        merger.write(joined_path)
-        merger.close()
-        
-        #az to nekdo bude chtit rozdelovat zpatky
-        with open( download_path +  f"/stranyprorozdeleni_uloha-{problem}.txt","w") as f:
-            json.dump(dictnarozdeleni,f)
+        merger_czech.write(joined_path_czech)
+        merger_foreign.write(joined_path_foreign)
+        merger_czech.close()
+        merger_foreign.close()
 
-        #tady uz jen vypisuju co se delo
-        #print('Suma:   ', sumpg)
-        #print('Whites: ',sum_whites)
-        #print('S+W:    ',sumpg+sum_whites)
+        with open(download_path + f"/stranyprorozdeleni_uloha-{problem}_czech.txt", "w") as f:
+            json.dump(dictnarozdeleni_czech, f)
+        with open(download_path + f"/stranyprorozdeleni_uloha-{problem}_foreign.txt", "w") as f:
+            json.dump(dictnarozdeleni_foreign, f)
 
-        jp = get_pages(joined_path)
-        print('Joined pages: ', jp)
+        jp_czech = get_pages(joined_path_czech)
+        jp_foreign = get_pages(joined_path_foreign)
+        print(f'Joined pages (Czech): {jp_czech}')
+        print(f'Joined pages (Foreign): {jp_foreign}')
         print()
 
         if split_exceptions:
-            #print(f'Exceptions u{problem}:')
-
             exc_path = download_path + f'/exceptions/uloha-{problem}'
             if not os.path.exists(exc_path):
                 os.makedirs(exc_path)
 
             for exception in exceptions:
-                #print(exception)
-
-                #narve jim do jmena souboru slovo exceptions misto ulohy a tim je premisti do slozky exceptions
                 a = exception.find(f'uloha')
                 os.rename(exception, exc_path + exception[a+7:])
 
-            exceptions = [] #reset pro kazdou ulohu
-
+            exceptions = []
 
     if not split_exceptions:
         print('Exceptions:')
-
         exc_path = download_path + f'/exceptions'
         if not os.path.exists(exc_path):
             os.mkdir(exc_path)
 
         for exception in exceptions:
             print(exception)
-
             a = exception.find(f'uloha')
-            #narve jim do jmena souboru slovo exceptions misto ulohy a tim je premisti do slozky exceptions
             os.rename(exception, exc_path + exception[a+7:])     
 
 if __name__ == "__main__":
-
-    problems = ["1","2","3","4","5","P","E","S"]  
+    problems = ["1", "2", "3", "4", "5", "P", "E", "S"]  
 
     rocnik = input('napis cislo rocniku: ')
     serie = input('napis cislo serie: ')
--- pdfsplit.py
+++ pdfsplit.py
@@ -1,9 +1,10 @@
-import os, json, shutil
+import os
+import json
+import shutil
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from collections import OrderedDict
-import pandas as pd
 
-def init_upload_me(split_path, upload_me_path, problems, upload_foreign_only, submitids = None):
+def init_upload_me(split_path, upload_me_path, problems, upload_foreign_only):
     back_up_path = upload_me_path + "/upload_me"
     if not os.path.exists(back_up_path):
         print("Vytvarim ./upload_me")
@@ -11,81 +12,72 @@ def init_upload_me(split_path, upload_me_path, problems, upload_foreign_only, su
         # create the back_up_path directory
         os.makedirs(back_up_path)
 
-        if upload_foreign_only and submitids is not None:
-            for problem in problems:
-                all_files_for_problem = os.listdir(split_path + f'/uloha-{problem}')
-                for file in all_files_for_problem:
-                    file_sumbitid = file[file.find("__")+2:-4] # the -4 is to remove the .pdf
-                    try:
-                        file_sumbitid = int(file_sumbitid)
-                    except:
-                        print(f"file_sumbitid: {file_sumbitid} neni cislo, preskakuji")
-                        continue
-                    # print(f"file_sumbitid: {file_sumbitid}")
-                    
-                    if file_sumbitid in submitids:
-                        # if the folder does not exist, create it
-                        if not os.path.exists(back_up_path + f'/uloha-{problem}'):
-                            os.makedirs(back_up_path + f'/uloha-{problem}')
-                        shutil.copyfile(split_path + f'/uloha-{problem}/' + file, back_up_path + f'/uloha-{problem}/' + file)
-                        # print(f"Copying {split_path + f'/uloha-{problem}/' + file} to {back_up_path + f'/uloha-{problem}/' + file}")                
-            
-        else:
-            if submitids is None:
-                print("Zadano ze chces jen zahranicni, ale submitids nenalezeno -> ponechavam vsechny soubory")
-            for problem in problems:
-                shutil.copytree(split_path + f'/uloha-{problem}', back_up_path + f'/uloha-{problem}')
+        for problem in problems:
+            problem_path = split_path + f'/uloha-{problem}'
+            foreign_path = problem_path + '_foreign'
+            czech_path = problem_path + '_czech'
+
+            if upload_foreign_only:
+                if os.path.exists(foreign_path):
+                    shutil.copytree(foreign_path, back_up_path + f'/uloha-{problem}')
+            else:
+                # Copy both Czech and foreign files
+                if os.path.exists(czech_path):
+                    shutil.copytree(czech_path, back_up_path + f'/uloha-{problem}')
+                if os.path.exists(foreign_path):
+                    shutil.copytree(foreign_path, back_up_path + f'/uloha-{problem}')
         
         print("upload_me vytvoren.\n")
     else:
         print("upload_me uz existuje.\n")
-
 
 def split_it(joined_and_split_dir, stranytxt_dir, problems):
     stranysouhlasi = True
     kdenesouhlasi = []
     
     for problem in problems:
+        for file_type in ['czech', 'foreign']:
+            split_path = joined_and_split_dir + f'/zaloha_split/uloha-{problem}_{file_type}'
+            joined_path = joined_and_split_dir + f'/joined_uloha-{problem}_{file_type}.pdf' 
+            stranytxtpath = stranytxt_dir + f"/stranyprorozdeleni_uloha-{problem}_{file_type}.txt"
 
-        split_path = joined_and_split_dir + f'/zaloha_split/uloha-{problem}'
-        joined_path = joined_and_split_dir + f'/joined_uloha-{problem}.pdf' 
-        stranytxtpath = stranytxt_dir + f"/stranyprorozdeleni_uloha-{problem}.txt"
+            print(f'uloha: {problem} - {file_type}')
 
-        print(f'uloha: {problem}')
+            if not os.path.exists(split_path):
+                os.makedirs(split_path)
 
-        if not os.path.exists(split_path):
-            os.makedirs(split_path)
+            if not os.path.exists(joined_path):
+                print(f"Joined file for problem {problem} - {file_type} does not exist. Skipping.")
+                continue
 
-        #nacteme ulozeny pocet stran
-        with open(stranytxtpath,"r") as f:
-            dictnarozdeleni = OrderedDict(json.load(f))
+            #nacteme ulozeny pocet stran
+            with open(stranytxtpath,"r") as f:
+                dictnarozdeleni = OrderedDict(json.load(f))
 
-        
-        with open(joined_path, "rb") as joinedf:
-            reader = PdfFileReader(joinedf)
-            joinedpages = reader.getNumPages()
-            print(f"Pocet stran joined u{problem}: {joinedpages}")
-        
-            pozice = 0
+            with open(joined_path, "rb") as joinedf:
+                reader = PdfFileReader(joinedf)
+                joinedpages = reader.getNumPages()
+                print(f"Pocet stran joined u{problem} - {file_type}: {joinedpages}")
+            
+                pozice = 0
 
-            for pdf in dictnarozdeleni.keys():
-                pages = dictnarozdeleni[pdf]
-                outpath = split_path + pdf[pdf.find("uloha-")+7:]
-                #print(str(pages).ljust(8), str(pozice).ljust(5), pdf )
+                for pdf in dictnarozdeleni.keys():
+                    pages = dictnarozdeleni[pdf]
+                    outpath = os.path.join(split_path, os.path.basename(pdf))
 
-                writer = PdfFileWriter()
-                for i in range(pages):
-                    writer.addPage(reader.getPage(pozice))
-                    pozice += 1 #posouvame pozici
+                    writer = PdfFileWriter()
+                    for i in range(pages):
+                        writer.addPage(reader.getPage(pozice))
+                        pozice += 1 #posouvame pozici
 
-                with open(outpath,"wb") as outf:
-                    writer.write(outf)
-            print(f"Celkovy pocet stran vytvorenych pdf u{problem}: {pozice}")
-            print()
+                    with open(outpath,"wb") as outf:
+                        writer.write(outf)
+                print(f"Celkovy pocet stran vytvorenych pdf u{problem} - {file_type}: {pozice}")
+                print()
 
-        if pozice !=  joinedpages:
-            stranysouhlasi = False
-            kdenesouhlasi.append(problem)
+            if pozice != joinedpages:
+                stranysouhlasi = False
+                kdenesouhlasi.append(f"{problem} - {file_type}")
 
     if stranysouhlasi:
         print("Strany sedi")
@@ -94,23 +86,15 @@ def split_it(joined_and_split_dir, stranytxt_dir, problems):
         print(kdenesouhlasi)
 
 if __name__ == "__main__":
-    
     problems = ["1","2","3","4","5","P","E","S"]
 
     rocnik = input('napis cislo rocniku: ')
     serie = input('napis cislo serie: ')
     upload_foreign_only = input('Chces uploadovat jen zahranicni (z) nebo vse (v)?')
-    if upload_foreign_only == "z":
-        upload_foreign_only = True
-    else:
-        upload_foreign_only = False
+    upload_foreign_only = (upload_foreign_only.lower() == "z")
 
     joined_and_split_dir = f'./corrected/rocnik{rocnik}/serie{serie}'
     stranytxt_dir = f"./download/rocnik{rocnik}/serie{serie}"
 
     split_it(joined_and_split_dir, stranytxt_dir, problems)
-    
-    submitids = pd.read_csv(joined_and_split_dir + '/submitids.csv',sep=';')
-    submitids = submitids["submit"].values
-
-    init_upload_me(joined_and_split_dir + f'/zaloha_split', joined_and_split_dir, problems, upload_foreign_only, submitids)
+    init_upload_me(joined_and_split_dir + f'/zaloha_split', joined_and_split_dir, problems, upload_foreign_only)
